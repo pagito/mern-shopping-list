3b10328c748b34277124046d8f00eaa0d1aafb0f
Giorgi Javrishvili <giorgi.javrishvili@gmail.com>
2019-09-01 15:22:10 +0800

    Mongo

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://localhost:27017/mern_shopping'
+	//mongoURI: 'mongodb://localhost:27017/mern_shopping'
+	mongoURI: 'mongodb+srv://pagito:pagitoDBUser@cluster0-tzrb3.mongodb.net/test?retryWrites=true&w=majority'
 };
