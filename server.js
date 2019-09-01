2e2fc2381a7c285866febc13d348147882ce0fde
Giorgi Javrishvili <giorgi.javrishvili@gmail.com>
2019-09-01 17:19:11 +0800

    Create auth

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const path = require("path");
+const config = require("config");
 
-const items = require('./routes/api/items');
+//const items = require('./routes/api/items');
+//const users = require('./routes/api/users');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+// bodyParser is included by default
+app.use(express.json());
 
 // DB Config
-const db= require('./config/keys').mongoURI;
+//const db= require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-	.connect(db, { useNewUrlParser: true })
+	.connect(db, { useNewUrlParser: true, useCreateIndex: true })
 	.then(() => console.log('MongoDB Connected...'))
 	.catch(err => console.log(err));
 
 // User Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
